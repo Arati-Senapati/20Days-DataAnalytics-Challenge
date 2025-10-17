dd922236fcfbf646e5fc0b4249642b6e55a12b0b
Arati-Senapati <aratisenapatissska@gmail.com>
2025-10-17 11:53:05 +0530

    DAY 1

---
+++ PYTHON/Day-1.py
@@ -0,0 +1,38 @@
+print("hello world")
+print("This is the first program")
+print("I hope u all like it")
+
+print("""hello world
+this is my first program
+I hope u all like it""")
+
+print("hello world \nthis is my first program\nI hope u all like it")
+
+#userinput
+"""name=input("Enter your name:")
+print(name)
+age=int(input("Enter your age:"))
+print(age)
+percentage=float(input("Enter your percentage:"))
+print(percentage)
+evaluate=eval(input("Enter evaluate expression:"))
+print(evaluate)"""
+
+#typecasting
+name = "arati"
+print(type(name))
+
+age=24
+print(type(age))
+
+a="123"
+b=456.83
+print(type(a))
+print(type(b))
+
+a=int(a)
+print("after conversion",type(a))
+
+s=a+b
+print(s)
+print(type(s))
---
+++ PYTHON/program solving-1.py
@@ -0,0 +1,53 @@
+#Write a program to display a person's name,age and address in three different lines.
+'''name="arati"
+age=24
+address="I.G Market,Burla"
+print(name)
+print(age)
+print(address)
+
+#Write a program to swap two variables.
+#Method1
+x=12
+y=13
+temp=x
+x=y
+y=temp
+print("x:",x)
+print("y:",y)
+
+#Method2
+a=12
+b=13
+print("a:",a)
+print("b:",b)
+
+a,b=b,a
+print("a:",a)
+print("b:",b)
+
+#Write a program to convert a float into integer.
+a=34.56
+print(type(a))
+
+a=int(a)
+print(type(a))
+#Write a program to take details from a student for ID-card and then print it in different lines.
+name=input("enter your name:")
+grade=input("enter your grade:")
+age=int(input("enter your age:"))
+address=input("enter your address:")
+email=input("enter your email:")
+ph_no=input("enter your ph_no:")
+print("Student Identity Card")
+print(name)
+print(age)
+print(address)
+print(email)
+print(ph_no)'''
+#Write a program to take an user input as integer then convert to float.
+number=int(input("enter your integer number:"))
+print(type(number))
+number=float(number)
+print(number)
+print(type(number))
